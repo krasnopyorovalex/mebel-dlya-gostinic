96b290f37cb3a0c01bdc407bc402f543a5755df8
Alex Krasnopyorov <krasnopyorovalex88@gmail.com>
2018-10-16 10:35:55 +0300

    add width 100% to slider image and remove target blank from link in menu

--- resources/views/layouts/partials/footer_menu.blade.php
+++ resources/views/layouts/partials/footer_menu.blade.php
@@ -1,6 +1,6 @@
 <h6>Нижнее меню</h6>
 <ul class="list-xxs">
     @foreach($menu->get('menu_footer') as $item)
-    <li><a target="_blank"  href="{{ $item->link }}">{{ $item->name }}</a></li>
+    <li><a href="{{ $item->link }}">{{ $item->name }}</a></li>
     @endforeach
 </ul>
--- resources/views/layouts/partials/header_menu.blade.php
+++ resources/views/layouts/partials/header_menu.blade.php
@@ -1,16 +1,16 @@
 <ul class="rd-navbar-nav">
     @foreach($menu->get('menu_main') as $item)
         <li>
-            <a target="_blank" href="{{ $item->link }}">{{ $item->name }}</a>
+            <a href="{{ $item->link }}">{{ $item->name }}</a>
             @if (count($item->menuItems))
                 <ul class="rd-navbar-dropdown">
                     @foreach ($item->menuItems as $subItem)
                         <li>
-                            <a target="_blank" href="{{ $subItem->link }}">{{ $subItem->name }}</a>
+                            <a href="{{ $subItem->link }}">{{ $subItem->name }}</a>
                             @if (count($subItem->menuItems))
                                 <ul class="rd-navbar-dropdown">
                                     @foreach ($subItem->menuItems as $subSubItem)
-                                        <li><a target="_blank"  href="{{ $subSubItem->link }}">{{ $subSubItem->name }}</a></li>
+                                        <li><a href="{{ $subSubItem->link }}">{{ $subSubItem->name }}</a></li>
                                     @endforeach
                                 </ul>
                             @endif
